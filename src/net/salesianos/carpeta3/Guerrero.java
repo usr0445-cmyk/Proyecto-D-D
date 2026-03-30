3ad0764be2aac013e8b0278acdc89b229aa1ee5e
Wilson Riascos <usr0445@salesianos-lacuesta.net>
2026-03-30 14:22:44 +0100

    Clase Guerrero terminado

--- src/net/salesianos/carpeta3/Guerrero.java
+++ src/net/salesianos/carpeta3/Guerrero.java
@@ -1,5 +1,21 @@
 package net.salesianos.carpeta3;
 
-public class Guerrero {
+public class Guerrero extends Personaje {
+
+    private String arma;
+
+    public Guerrero(String nombre, int nivel, String arma){
+        super(nombre, nivel);
+        this.arma = arma;
+    }
+
+    @Override
+    public void mostrarInfo(){
+        super.mostrarInfo();
+        System.out.println("Arma: " + arma);
+    }
     
+    public void ataquePesado(){
+        gastarEnergia(15);
+    }
 }
